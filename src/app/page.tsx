14abc744bd5c18c5352022ee1d9aed9e2eec9d6f
zackpaton <91792781+zackpaton@users.noreply.github.com>
2025-06-30 22:12:10 -0400

    Fixed Build Errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       <div className="container mx-auto px-4 py-8 bg-gray-100">
         <h1 className="text-3xl font-bold mb-4 text-slate-900">Welcome to my website 👋</h1>
         <p className="text-red-500">
-          I'm Zack!
+          I&apos;m Zack!
         </p>
         <p className="test-bg">
           testing background
